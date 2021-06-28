2a0f19135f141276a47fa7660aafe7fea7e69eca
Fikret IŞILDAK <isildakfikret@gmail.com>
2021-06-28 10:03:18 +0300

    initial commit

---
+++ src/test/java/com/example/springboot/sqliteboilerplate/SqliteBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.sqliteboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliteBoilerplateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
