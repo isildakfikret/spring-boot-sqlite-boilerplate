3be1ecb12735d4872d7ca74a2bc843b486640de0
Fikret IŞILDAK <isildakfikret@gmail.com>
2021-06-28 10:35:00 +0300

    add jdbc-template tests

--- src/test/java/com/example/springboot/sqliteboilerplate/config/DbConfigTest.java
+++ src/test/java/com/example/springboot/sqliteboilerplate/config/DbConfigTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -17,12 +18,16 @@ class DbConfigTest {
 
   @Autowired private DataSource dataSource;
   @Autowired private DataSource customDataSource;
+  @Autowired private JdbcTemplate jdbcTemplate;
+  @Autowired private JdbcTemplate customJdbcTemplate;
 
   @Test
-  @DisplayName("data sources should be created when the app started")
+  @DisplayName("data sources and jdbc-templates should be created when the app started")
   void contextLoads() {
     assertThat(this.dataSource).isNotNull();
     assertThat(this.customDataSource).isNotNull();
+    assertThat(this.jdbcTemplate).isNotNull();
+    assertThat(this.customJdbcTemplate).isNotNull();
   }
 
 }
